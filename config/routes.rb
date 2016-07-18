862fca847ce2486752347275b5a275bc1682d820
aaronnke <aaronnke@gmail.com>
2016-07-18 16:04:36 +0800

    asd

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :users, controller: :users, only: :create
 
-root 'users#home'
+  root 'users#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
