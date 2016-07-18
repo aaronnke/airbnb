d893af2e278104d23f3a12487f555486544dcd70
aaronnke <aaronnke@gmail.com>
2016-07-18 21:40:13 +0800

    asd

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,22 @@
 class User < ActiveRecord::Base
   include Clearance::User
 
+has_many :authentications, :dependent => :destroy
+
+  def self.create_with_auth_and_hash(authentication,auth_hash)
+    create! do |u|
+      u.name = auth_hash["info"]["first_name"]
+      u.email = auth_hash["extra"]["raw_info"]["email"]
+      u.authentications<<(authentication)
+    end
+  end
+
+  def fb_token
+    x = self.authentications.where(:provider => :facebook).first
+    return x.token unless x.nil?
+  end
+
+  def password_optional?
+    true
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,14 @@ Rails.application.routes.draw do
 
   resources :users, controller: :users, only: :create
 
+  resources :users, only: [:show, :edit, :update, :destroy]
+
   root 'users#home'
+
+  get "/auth/:provider/callback" => "sessions#create_from_omniauth"
+
+  get '/resetpassword' => 'clearance/passwords#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
