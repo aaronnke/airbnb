a37dd3214ec65806fe55af693b271bf5b3ffcaf5
aaronnke <aaronnke@gmail.com>
2016-07-29 15:25:49 +0800

    asd

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-      provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+      provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
       	scope: 'name,email,image', display: 'popup'
 end
